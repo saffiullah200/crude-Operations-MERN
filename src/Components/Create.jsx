cc7d08147321b2d9b77d956decc2b64ba79f1444
saffiullah200 <42832684+saffiullah200@users.noreply.github.com>
2019-01-30 23:39:53 +0500

    form registration

---
+++ src/Components/Create.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+
+export default class Create extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangePersonName = this.onChangePersonName.bind(this);
+    this.onChangeBusinessName = this.onChangeBusinessName.bind(this);
+    this.onChangeGstNumber = this.onChangeGstNumber.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      person_name: "",
+      business_gst_number: "",
+      business_name: ""
+    };
+  }
+
+  onChangePersonName(e) {
+    this.setState({ person_name: e.target.value });
+  }
+  onChangeBusinessName(e) {
+    this.setState({ business_name: e.target.vale });
+  }
+
+  onChangeGstNumber(e) {
+    this.setState({
+      business_gst_number: e.target.value
+    });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    this.setstate = {
+      person_name: "",
+      business_gst_number: "",
+      business_name: ""
+    };
+  }
+
+  render() {
+    return (
+      <div style={{ marginTop: 10 }}>
+        <h3>Add New Business</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Person Name: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.person_name}
+              onChange={this.onChangePersonName}
+            />
+          </div>
+          <div className="form-group">
+            <label>Business Name: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.business_name}
+              onChange={this.onChangeBusinessName}
+            />
+          </div>
+          <div className="form-group">
+            <label>GST Number: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.business_gst_number}
+              onChange={this.onChangeGstNumber}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Register Business"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ src/Components/Edit.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Edit extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>Welcome to Edit Component!!</p>
+      </div>
+    );
+  }
+}
+
+export default Edit;
